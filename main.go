1cf8fe8ce34efcf4a4c410caf51ffe54cb60c42a
Avindra Goolcharan <aavindraa@gmail.com>
2021-01-19 15:37:14 -0500

    cleanup switch

--- main.go
+++ main.go
@@ -14,21 +14,19 @@ func main() {
 		args = args[1:]
 	}
 
-	switch len(args) {
-	case 2:
-	case 1:
+	if len(args) > 0 {
 		arg0 := args[0]
 		if dirp.IsDir(arg0) {
 			cfg := dirp.FindDirs(arg0)
 			dirp.Selector(cfg)
 		} else if arg0 == "hook" {
-			if len(args) == 2 && args[1] == "bash" {
+			if len(args) >= 2 && args[1] == "bash" {
 				dirp.PrintBashHook()
 			} else {
 				dirp.PrintHook() // fish
 			}
 		}
-	default:
+	} else {
 		if dirp.InputHasData() {
 			dirp.Feeder()
 			return
