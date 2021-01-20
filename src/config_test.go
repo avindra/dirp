93a4f18a232029b52bd75fd99880705d8200414f
Avindra Goolcharan <aavindraa@gmail.com>
2021-01-19 23:03:54 -0500

    Add tests for comments

--- src/config_test.go
+++ src/config_test.go
@@ -24,3 +24,32 @@ Downloads|/home/dolores/Downloads
 		t.Error("Corrupted parse")
 	}
 }
+
+func TestConfigComments(t *testing.T) {
+	cfg := ReadConfig(strings.NewReader(`
+rootfs|/
+sys library|/Library
+#temp fs|/tmp
+`))
+
+	if len(cfg) > 2 {
+		t.Error("Too many entries")
+	}
+}
+
+// commented lines MUST begin with a #
+func TestConfigCommentLimitation(t *testing.T) {
+	cfg := ReadConfig(strings.NewReader(`
+rootfs|/
+sys library|/Library
+ #temp fs|/tmp
+`))
+
+	if len(cfg) != 3 {
+		t.Error("Too many entries")
+	}
+
+	if cfg[" #temp fs"] != "/tmp" {
+		t.Error("Boo")
+	}
+}
