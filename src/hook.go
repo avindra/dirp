83305097855ccd1873a2e88e5167ed47cbae7815
Avindra Goolcharan <aavindraa@gmail.com>
2025-04-26 16:40:19 -0400

    use os.Stdout.Write instead of print

--- src/hook.go
+++ src/hook.go
@@ -2,6 +2,7 @@ package dirp
 
 import (
 	"embed"
+	"os"
 )
 
 //go:embed hooks/*
@@ -10,23 +11,23 @@ var folder embed.FS
 // PrintHook emits shell code for Bash, ZSH, sh, BusyBox, etc
 func PrintHook() {
 	hook, _ := folder.ReadFile("hooks/hook.sh")
-	print(string(hook))
+	os.Stdout.Write(hook)
 }
 
 // PrintFishHook emits shell code for Fish
 func PrintFishHook() {
 	hook, _ := folder.ReadFile("hooks/hook.fish")
-	print(string(hook))
+	os.Stdout.Write(hook)
 }
 
 // PrintRcHook emits code for rc, the plan 9 shell
 func PrintRcHook() {
 	hook, _ := folder.ReadFile("hooks/hook.rc")
-	print(string(hook))
+	os.Stdout.Write(hook)
 }
 
 // PrintEsHook emits code for es, a shell based on rc
 func PrintEsHook() {
 	hook, _ := folder.ReadFile("hooks/hook.es")
-	print(string(hook))
+	os.Stdout.Write(hook)
 }
