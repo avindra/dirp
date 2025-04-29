2b730e47caba04c048dfbe076c597462b736f6b3
Avindra Goolcharan <aavindraa@gmail.com>
2025-04-29 18:28:26 -0400

    cleanup: refactor out goto, remove go run -- compat

--- main.go
+++ main.go
@@ -16,43 +16,40 @@ func main() {
 	args := os.Args
 	args = args[1:]
 
-	if len(args) > 0 {
-		// compat with go run . --
-		if args[0] == "--" {
-			args = args[1:]
-			if len(args) == 0 {
-				goto NOARGS
-			}
-		}
-
-		arg0 := args[0]
-		if dirp.IsDir(arg0) {
-			driller(arg0)
-		} else if arg0 == "hook" {
-			if len(args) >= 2 {
-				switch args[1] {
-				case "fish":
-					dirp.PrintFishHook()
-				case "pwsh":
-					dirp.PrintPwshHook()
-				case "es":
-					dirp.PrintEsHook()
-				case "rc":
-					dirp.PrintRcHook()
-				default:
-					panic("I don't know about " + args[1])
-				}
-			} else {
-				dirp.PrintHook()
-			}
-		} else if arg0 == "cfg" {
-			fmt.Print(dirp.GetConfigPath())
-			os.Exit(2)
-		}
+	if len(args) == 0 {
+		handleNoArgs()
 		return
 	}
 
-NOARGS:
+	arg0 := args[0]
+	if dirp.IsDir(arg0) {
+		driller(arg0)
+	} else if arg0 == "hook" {
+		if len(args) >= 2 {
+			switch args[1] {
+			case "fish":
+				dirp.PrintFishHook()
+			case "pwsh":
+				dirp.PrintPwshHook()
+			case "es":
+				dirp.PrintEsHook()
+			case "rc":
+				dirp.PrintRcHook()
+			default:
+				panic("I don't know about " + args[1])
+			}
+		} else {
+			dirp.PrintHook()
+		}
+	} else if arg0 == "cfg" {
+		fmt.Print(dirp.GetConfigPath())
+		os.Exit(2)
+	} else {
+		handleNoArgs()
+	}
+}
+
+func handleNoArgs() {
 	var cfg dirp.ConfigSelection
 	if dirp.InputHasData() {
 		cfg = dirp.ReadConfig(os.Stdin)
